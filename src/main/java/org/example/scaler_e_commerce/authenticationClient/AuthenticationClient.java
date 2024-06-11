1b2afd834e948d61f516b3a315bc730597a3db20
Ayush <ayushagarwal.dev@gmail.com>
2024-06-11 19:56:13 +0530

    add service discovery and load balance code

--- src/main/java/org/example/scaler_e_commerce/authenticationClient/AuthenticationClient.java
+++ src/main/java/org/example/scaler_e_commerce/authenticationClient/AuthenticationClient.java
@@ -3,7 +3,6 @@ package org.example.scaler_e_commerce.authenticationClient;
 import org.example.scaler_e_commerce.authenticationClient.dtos.ValidateResponseDto;
 import org.example.scaler_e_commerce.authenticationClient.dtos.ValidateTokenRequestDto;
 import org.example.scaler_e_commerce.config.CustomProperties;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,20 +10,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class AuthenticationClient {
 
-    private final RestTemplateBuilder restTemplateBuilder;
-
+    private final RestTemplate restTemplate;
     private final CustomProperties customProperties;
 
 
-    public AuthenticationClient(RestTemplateBuilder restTemplateBuilder, CustomProperties customProperties) {
-        this.restTemplateBuilder = restTemplateBuilder;
-
+    public AuthenticationClient(RestTemplate restTemplate, CustomProperties customProperties) {
+        this.restTemplate = restTemplate;
         this.customProperties = customProperties;
     }
 
 
     public ValidateResponseDto validate(String token) {
-        RestTemplate restTemplate = restTemplateBuilder.build();
         ValidateTokenRequestDto validateTokenRequestDto = new ValidateTokenRequestDto();
         validateTokenRequestDto.setToken(token);
         ResponseEntity<ValidateResponseDto> request = restTemplate.postForEntity(customProperties.getUserserviceUrl() + "/auth/validate", validateTokenRequestDto, ValidateResponseDto.class);
